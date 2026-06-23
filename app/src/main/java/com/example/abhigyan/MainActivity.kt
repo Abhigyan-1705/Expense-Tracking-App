4d95e8d1c23cb1fc865376a943339a000ed0cfe8
Abhigyan-1705 <abhigyank175@gmail.com>
2026-06-23 21:26:38 +0530

    Fixed and removed secrets

---
+++ app/src/main/java/com/example/abhigyan/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.abhigyan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.abhigyan.ui.theme.RupLoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RupLoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/abhigyan/RupLoApp.kt
@@ -0,0 +1,5 @@
+package com.example.abhigyan
+
+import android.app.Application
+
+class RupLoApp : Application()
